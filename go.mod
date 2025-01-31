de15e2e099c62aa3a1188b72bf19354f6008c6da
dodik_sukma <putudodik2000@gmail.com>
2025-01-31 23:14:06 +0700

    app-say-hello

---
+++ go.mod
@@ -0,0 +1,5 @@
+module app-say-hello
+
+go 1.23.5
+
+require github.com/DodikSukma/go-say-hello/v2 v2.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/DodikSukma/go-say-hello/v2" // Import the package to make it available in this package scope. This is necessary because Go doesn't allow importing packages with only init functions.
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello("Dodik Bagus Genjing"))
+}
